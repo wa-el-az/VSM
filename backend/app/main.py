9983c53613b46da8baabd9a4e981fe2e90ae7564
Wael Amrani Zerrifi <waelaz1983@gmail.com>
2026-05-13 19:35:44 +0100

    fix: resolve websocket connection failure by updating nginx and cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -118,7 +118,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] if settings.app_debug else [],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
